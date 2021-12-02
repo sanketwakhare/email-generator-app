c51224c122a4afd6b79b7a36ae283cadfa2235c7
Sanket <sanketwakhare@gmail.com>
2021-12-02 20:44:38 +0530

    working code

--- script.js
+++ script.js
@@ -16,21 +16,21 @@ const handleSubmit = () => {
 
     // generate patterns
     let patterns = [];
-    patterns.push(`${firstName}${lastName}@${domainName}${domain}`);
-    patterns.push(`${firstName}_${lastName}@${domainName}${domain}`);
-    patterns.push(`${firstName.charAt(0)}${lastName}@${domainName}${domain}`);
-    patterns.push(`${firstName.charAt(0)}.${lastName}@${domainName}${domain}`);
-    patterns.push(`${firstName.charAt(0)}_${lastName}@${domainName}${domain}`);
-    patterns.push(`${firstName}@${domainName}${domain}`);
-    patterns.push(`${lastName}@${domainName}${domain}`);
-    patterns.push(`${lastName}${firstName}@${domainName}${domain}`);
-    patterns.push(`${lastName}.${firstName.charAt(0)}@${domainName}${domain}`);
-    patterns.push(`${lastName.charAt(0)}${firstName}@${domainName}${domain}`);
-    patterns.push(`${lastName.charAt(0)}.${firstName}@${domainName}${domain}`);
-    patterns.push(`${lastName.charAt(0)}_${firstName}@${domainName}${domain}`);
-    patterns.push(`${firstName}${lastName.charAt(0)}@${domainName}${domain}`);
-    patterns.push(`${firstName}.${lastName.charAt(0)}@${domainName}${domain}`);
-    patterns.push(`${firstName}_${lastName.charAt(0)}@${domainName}${domain}`);
+    patterns.push(firstName + lastName + '@' + domainName + domain);
+    patterns.push(firstName + '_' + lastName + '@' + domainName + domain);
+    patterns.push(firstName.charAt(0) + lastName + '@' + domainName + domain);
+    patterns.push(firstName.charAt(0) + '.' + lastName + '@' + domainName + domain);
+    patterns.push(firstName.charAt(0) + '_' + lastName + '@' + domainName + domain);
+    patterns.push(firstName + '@' + domainName + domain);
+    patterns.push(lastName + '@' + domainName + domain);
+    patterns.push(lastName + firstName + '@' + domainName + domain);
+    patterns.push(lastName + '.' + firstName.charAt(0) + '@' + domainName + domain);
+    patterns.push(lastName.charAt(0) + firstName + '@' + domainName + domain);
+    patterns.push(lastName.charAt(0) + '.' + firstName + '@' + domainName + domain);
+    patterns.push(lastName.charAt(0) + '_' + firstName + '@' + domainName + domain);
+    patterns.push(firstName + lastName.charAt(0) + '@' + domainName + domain);
+    patterns.push(firstName + '.' + lastName.charAt(0) + '@' + domainName + domain);
+    patterns.push(firstName + '_' + lastName.charAt(0) + '@' + domainName + domain);
 
 
     // remove all event listeners
@@ -60,9 +60,14 @@ const handleSubmit = () => {
         let copyBtn = document.createElement('input');
         copyBtn.setAttribute('id', 'btn-copy-' + index);
         copyBtn.setAttribute('value', 'Copy');
+        copyBtn.classList.add('btn');
+        copyBtn.classList.add('btn-outline-primary');
+        copyBtn.classList.add('pattern-container');
+        copyBtn.classList.add('pattern-container');
+        //  'link-secondary', 'pattern-container']);
         copyBtn.setAttribute('type', 'button');
         copyBtn.addEventListener('click', handleCopy);
-        copyBtn.className = 'pattern-container';
+        // copyBtn.classList.add(['pattern-container']);
         patternContainer.appendChild(copyBtn);
 
         outputElement.appendChild(patternContainer);
@@ -81,6 +86,24 @@ const handleCopy = (event) => {
     const textCopied = event.target.parentElement.innerText;
     /* Copy the text on clip board */
     navigator.clipboard.writeText(textCopied);
+
+    let msgEle = document.createElement("div");
+    msgEle.setAttribute('role', 'alert');
+    msgEle.classList.add('alert');
+    msgEle.classList.add('alert-success');
+    msgEle.classList.add('d-inline');
+    msgEle.classList.remove('d-none');
+    msgEle.innerText = textCopied + ' copied on clipboard!';
+
+    event.target.parentElement.appendChild(msgEle);
+
+
+    setTimeout(() => {
+        // hide toast
+        if (event.target.parentElement.hasChildNodes()) {
+            event.target.parentElement.removeChild(event.target.parentElement.childNodes[event.target.parentElement.childNodes.length - 1]);
+        }
+    }, 3000);
 
 
 }
